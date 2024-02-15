6a2c23a4c5d81061fc041fbe2cd69bcbb5d6abbe
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-02-15 11:15:02 +0330

    Cleaning the input after a request

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,28 @@
 import { Search as SearchIcon } from "react-bootstrap-icons";
 import s from "./style.module.css";
+import { useState } from "react";
 const SearchBar = ({ onSubmit }) => {
+  const [value, setValue] = useState("");
   const submit = (e) => {
     if (e.key === "Enter" && e.target.value.trim() !== "") {
       onSubmit(e.target.value);
+      setValue("");
     }
     console.log(e.target.value);
   };
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
   return (
     <>
       <SearchIcon className={s.icon} size={27} />
       <input
         onKeyUp={submit}
         type="text"
+        onChange={handleChange}
+        value={value}
         className={s.input}
         placeholder={"Search a tv show you may like"}
       />
